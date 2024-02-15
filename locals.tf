94e3f99a4cf40278b63df8acfb81cd78fa633bb0
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2024-02-15 18:20:18 -0300

    feat: add dynamodb table creation option

--- locals.tf
+++ locals.tf
@@ -7,4 +7,6 @@ locals {
 
   subnet_ids         = var.lambda.network.subnets_tag != null ? data.aws_subnets.functions.ids : var.vpc_cidr_block != null ? [aws_subnet.public_subnet[0].id, aws_subnet.private_subnet[0].id] : null
   security_group_ids = var.lambda.network.security_groups_tag != null ? data.aws_security_groups.functions.ids : var.security_group_name != null ? [aws_security_group.lambda[0].id] : null
+
+  dynamodb_tables_arn = var.dynamodb_tables != null ? [for table in var.dynamodb_tables : aws_dynamodb_table.this[table.name].arn] : length(var.lambda.dynamodb_tables) > 0 ? [for table in var.lambda.dynamodb_tables : data.aws_dynamodb_table.table_name[table.name].arn] : null
 }
